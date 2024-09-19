9b1a62b1c9c8eb44b1c391c98d772805b873d5b7
DSM2 <fatec-dsm2@academico.intra>
2024-09-18 22:51:09 -0300

    Algumas mudanças no projeto

---
+++ SistemaLoja/controllers/PedidosController.js
@@ -0,0 +1,28 @@
+//Importando o express
+import express from 'express'
+//Escolhendo a variavel para ser o router
+const router = express.router()
+
+// ROTA PEDIDOS
+const pedidos = [ 
+    {
+    nump: 1,
+    valor: "388,80",
+    },
+    {
+      nump: 2,
+      valor: "3723,00",
+    },
+    {
+      nump: 3,
+      valor: "99,00",
+    },
+    {
+      nump: 4,
+      valor: "208,90",
+    },  
+  ]
+  res.render("pedidos", 
+  {
+  pedidos: pedidos
+  });
--- SistemaLoja/index.js
+++ SistemaLoja/index.js
@@ -1,46 +1,21 @@
-const express = require("express");
-
+// Importando o Express com ES6 Modules
+import express from 'express'
+//Iniciando o Express na variável app
 const app = express();
 
-app.set("view engine", "ejs");
+// Importando os Controllers (onde estão as rotas)
+import ClientesController from "./controllers/ClientesController.js"
 
+// Define o EJS como Renderizador de páginas
+app.set("view engine", "ejs");
+// Define o uso da pasta "public" para uso de arquivos estáticos
 app.use(express.static("public"));
+
+// Definindo o uso das rotas dos Controllers
+app.use("/", ClientesController)
 
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/clientes", (req, res) => {
-  const cliente = [
-    {
-      num: 1,
-      nome: "Carlos da Silva",
-      cpf: "458.115.770-60",
-      endereco: "Quadra QE 11 Área Especial C, nº435, Guará I. CEP: 71090-285. Brasília - Distrito Federal"
-    },
-    {
-      num: 2,
-      nome: "Joaquim Magno",
-      cpf: "648.453.620-15",
-      endereco: "Rua Cristiano Olsen, nº254, Jardim Sumaré. CEP: 16015-244. Araçatuba - São Paulo"
-    },
-    {
-      num: 3,
-      nome: "Charles Emmanuel",
-      cpf: "571.147.050-77",
-      endereco: "Rua da Imprensa, nº97, Monte Castelo. CEP: 79002-290. Campo Grande - Mato Grosso do Sul"
-    },
-    {
-      num: 4,
-      nome: "Guilherme Cabral",
-      cpf: "155.050.150-00",
-      endereco: "Rua Domingos Olímpio, nº42, Casa, Centro. CEP: 62011-140. Sobral - Ceará"
-    }
-  ];
-  res.render("clientes", {
-    cliente: cliente
-})
-
 });
 
 app.get("/pedidos", (req,res) => {
